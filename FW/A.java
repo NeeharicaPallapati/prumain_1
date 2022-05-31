f9e57303f1a1cd14bd2a4901f2baebdf91cf1361
Neeharica <neeharica.pallapati@gmail.com>
2022-05-31 19:05:05 +0530

    commiting code

---
+++ FW/A.java
@@ -0,0 +1,7 @@
+package Sample1;
+
+public class A {
+public int add(int x, int y){
+return (x + y);
+}
+}
---
+++ Prucom/C.java
@@ -0,0 +1,7 @@
+package Sample1;
+
+public class C {
+public int sub(int x, int y){
+return (x - y);
+}
+}
